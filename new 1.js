9a17219a13aa95498dc654e9a6e6afec9e7e7091
Ethan Kingrey <ekingrey19@ksu.edu>
2020-08-27 12:49:55 -0500

    first test

---
+++ new 1.js
@@ -0,0 +1,12 @@
+<script src="https://https://github.com/ekingrey/Vue-Test"></script>
+
+<div id="app">
+  {{ message }}
+</div>
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+
